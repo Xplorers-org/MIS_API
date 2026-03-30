fe2c3767a5184b31f42cf5f5afecbb779494e0ad
UdasriHasindu <usrimart@gmail.com>
2026-03-30 18:03:59 +0530

    add a lifespan to load models before api traffic

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
+from services import predictor
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    # Load models on startup
+
+    print("\n--- SYSTEM BOOT ---")
+    print("Pre-loading deep learning models from the Hub into RAM...\n")
+
+    predictor._get_wave_model()
+    predictor._get_spiral_model()    
+
+
+    print("Models successfully cached! Server is now ready to accept network traffic.\n")
+
+    yield
+    # Everything before 'yield' happens BEFORE the server accepts traffic.
+
+    print("\n--- SYSTEM SHUTDOWN ---")
+    print("Releasing memory and shutting down...")
+
+
+
 
 app = FastAPI(
     title="Motor Impairment Score API",
-    description="API for predicting Parkinson's motor impairment from drawings."
+    description="API for predicting Parkinson's motor impairment from drawings.",
+    lifespan=lifespan
 )
 
 @app.get("/")
